7d5bf4a0ec07e657b33b6211d9cbf997b7537888
gaboliveiradev <g38789138@gmail.com>
2022-09-28 15:08:36 -0300

    criando as propriedades do model componentes.cs

--- AppEtec/AppEtec/Models/Componente.cs
+++ AppEtec/AppEtec/Models/Componente.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace AppEtec.Models
 {
-    internal class Componente
+    public class Componente
     {
+        public string Nome { get; set; }
+        public int CargaHoraria { get; set; }
+        public string AtribuicoesResponsabilidades { get; set; }
+        public string ValoresAtitudes { get; set; }
+        public string Competencias { get; set; }
+        public string Habilidades { get; set; }
+        public string BasesTecnologicas { get; set; }
     }
 }

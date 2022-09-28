8f6484f37e15b4810c2bc0be609a46de845c956f
gaboliveiradev <g38789138@gmail.com>
2022-09-28 15:41:44 -0300

    correções de erro

--- AppEtec/AppEtec/MainPage.xaml.cs
+++ AppEtec/AppEtec/MainPage.xaml.cs
@@ -6,13 +6,17 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+using AppEtec.Views;
+
 namespace AppEtec
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : MasterDetailPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Inicial)));
         }
     }
 }

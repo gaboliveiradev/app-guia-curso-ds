75366cdb807760bdf0df3093f6e7055ff165d2b6
gaboliveiradev <g38789138@gmail.com>
2022-09-29 14:58:02 -0300

    criando os cliecked e os metodos referente a cada button

--- AppEtec/AppEtec/MainPage.xaml.cs
+++ AppEtec/AppEtec/MainPage.xaml.cs
@@ -18,5 +18,35 @@ namespace AppEtec
 
             Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Inicial)));
         }
+
+        private void btn_inicial(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_primeiro(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_segundo(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_terceiro(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_vestibulinho(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_contato(object sender, EventArgs e)
+        {
+
+        }
     }
 }

65471483a7394cec0418333d8908dc9802f840be
gaboliveiradev <g38789138@gmail.com>
2022-10-05 14:03:15 -0300

    criando os clickeds dos button no arquivo xaml e renomeando os metodos dos seguintes

--- AppEtec/AppEtec/Views/ComponentesPrimeiro.xaml.cs
+++ AppEtec/AppEtec/Views/ComponentesPrimeiro.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using AppEtec.Models;
 
 namespace AppEtec.Views
 {
@@ -15,6 +16,46 @@ namespace AppEtec.Views
         public ComponentesPrimeiro()
         {
             InitializeComponent();
+        }
+
+        private void btnLinguagem(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAlgoritmos(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBanco(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAnalise(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDesign(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnWeb(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFundamentos(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTecnicas(object sender, EventArgs e)
+        {
+
         }
     }
 }
